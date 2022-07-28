02c0b98191906091367e3e7894d5b26b9426a5aa
excaoxsal <exca561@gmail.com>
2022-07-28 16:22:47 +0700

    Fixing Banner in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,97 @@
 
 
 @section('content')
+<section class="slider_section ">
+   <div class="slider_bg_box">
+      <img src="{{url('image/Kayu_jati.jpeg')}}" alt="">
+   </div>
+   <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+         <div class="carousel-item active">
+            <div class="container ">
+               <div class="row">
+                  <div class="col-md-7 col-lg-6 ">
+                     <div class="detail-box">
+                        <h1 style="color: red">
+                           <span>
+                           Sale 20% Off
+                           </span>
+                           <br>
+                           On Everything
+                        </h1>
+                        <p style="color: red">
+                           Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                        </p>
+                        <div class="btn-box">
+                           <a href="" class="btn1">
+                           Shop Now
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="carousel-item ">
+            <div class="container ">
+               <div class="row">
+                  <div class="col-md-7 col-lg-6 ">
+                     <div class="detail-box">
+                        <h1>
+                           <span>
+                           Sale 20% Off
+                           </span>
+                           <br>
+                           On Everything
+                        </h1>
+                        <p>
+                           Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                        </p>
+                        <div class="btn-box">
+                           <a href="" class="btn1">
+                           Shop Now
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <div class="container ">
+               <div class="row">
+                  <div class="col-md-7 col-lg-6 ">
+                     <div class="detail-box">
+                        <h1>
+                           <span>
+                           Sale 20% Off
+                           </span>
+                           <br>
+                           On Everything
+                        </h1>
+                        <p>
+                           Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                        </p>
+                        <div class="btn-box">
+                           <a href="" class="btn1">
+                           Shop Now
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="container">
+         <ol class="carousel-indicators">
+            <li data-target="#customCarousel1" data-slide-to="0" class="active"></li>
+            <li data-target="#customCarousel1" data-slide-to="1"></li>
+            <li data-target="#customCarousel1" data-slide-to="2"></li>
+         </ol>
+      </div>
+   </div>
+</section>
 <section class="product_section layout_padding">
          <div class="container">
             <div class="heading_container heading_center">
