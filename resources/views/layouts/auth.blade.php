d744e6f5264be084dfe514782afbe1339dda87d2
excaoxsal <exca561@gmail.com>
2022-07-28 16:58:10 +0700

    Ubah Welcome

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -41,15 +41,9 @@
                             <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
                             <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();">Logout
                                         {{ __('Logout') }}
                                     </a>
 
